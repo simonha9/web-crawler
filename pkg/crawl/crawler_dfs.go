fdb842fb6425e1bf66f2c7b5648da580f8d63d66
Simon H <simon9ha@gmail.com>
2023-09-14 11:01:36 -0400

    feat: adding dfs crawler

--- pkg/crawl/crawler_dfs.go
+++ pkg/crawl/crawler_dfs.go
@@ -1,29 +1,58 @@
 package crawl
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/simonha9/web-crawler/pkg/downloader"
+)
+
+const MAX_DEPTH = 50
 
 type DFSCrawler struct {
 	seeds   []string
-	queue   []string
 	visited map[string]bool
+	downloader downloader.Downloader
 }
 
 func NewDFSCrawler(seeds []string) *DFSCrawler {
 	return &DFSCrawler{
 		seeds:   seeds,
-		queue:   make([]string, 0),
 		visited: make(map[string]bool),
 	}
 }
 
 func (c *DFSCrawler) Crawl(url string) {
-	c.queue = append(c.queue, url)
+	c.crawl(url, 0)
+}
 
-	for len(c.queue) > 0 {
-		url := c.queue[0]
-		c.queue = c.queue[1:]
-		fmt.Println(url)
-		// do something
-
+func (c *DFSCrawler) crawl(url string, depth int) {
+	if c.IsVisited(url) {
+		return
 	}
+
+	if depth > MAX_DEPTH {
+		return
+	}
+	
+	c.Visit(url)
+
+	links := c.downloader.DownloadAndGetLinks(url)
+	for _, link := range links {
+		for _, l := range link {
+			if strings.HasPrefix(l, "/") {
+				l = url + l
+			}
+			c.crawl(l, depth + 1)
+		}
+	}
+} 
+
+func (c DFSCrawler) Visit(url string) {
+	c.visited[url] = true
+	fmt.Println("Visited", url)
+}
+
+func (c DFSCrawler) IsVisited(url string) bool {
+	return c.visited[url]
 }
